a7f218c22b24fb98242084b1de7c806d0442b560
Shaoting <shaotingf@uchicago.edu>
2025-01-03 15:32:54 -0600

    fix format

--- .buildkite/drawing_wrapper.py
+++ .buildkite/drawing_wrapper.py
@@ -6,7 +6,8 @@ import argparse
 
 sys.path.append('/local/shaotingf/lmcache1/lmcache-tests/outputs')
 spec = importlib.util.spec_from_file_location(
-    "process_result", "/local/shaotingf/lmcache1/lmcache-tests/outputs/process_result.py"
+    "process_result", 
+    "/local/shaotingf/lmcache1/lmcache-tests/outputs/process_result.py"
 )
 process_result = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(process_result)
@@ -24,11 +25,16 @@ def process_all_csv_in_directory(directory):
         process_result.process_result_file(csv_file)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process CSV files in a directory")
-    parser.add_argument("output", type=str, help="The subdirectory to process under lmcache-tests")
+    parser = argparse.ArgumentParser(
+        description="Process CSV files in a directory"
+    )
+    parser.add_argument(
+        "output", type=str, 
+        help="The subdirectory to process under lmcache-tests"
+    )
     args = parser.parse_args()
 
     # Specify the directory where the CSV files are located
-    directory = os.path.dirname(os.path.realpath(__file__))  # This gets the current directory
+    directory = os.path.dirname(os.path.realpath(__file__)) 
     directory_to_process = os.path.join(directory, "lmcache-tests", args.output)
     process_all_csv_in_directory(directory_to_process)
