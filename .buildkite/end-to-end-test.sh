545afcb2f3126304c41d34f4288640a74e22c291
Shaoting <shaotingf@uchicago.edu>
2025-01-03 15:21:27 -0600

    fix names

--- .buildkite/end-to-end-test.sh
+++ .buildkite/end-to-end-test.sh
@@ -5,5 +5,5 @@ python3 main.py tests/tests.py -f test_chunk_prefill -o outputs/
 python3 main.py tests/tests.py -f test_lmcache_local_gpu -o outputs/
 python3 main.py tests/tests.py -f test_lmcache_local_distributed -o outputs/
 python3 main.py tests/tests.py -f test_lmcache_remote_cachegen -o outputs/
-cd ../multi-round-qa/.buildkite
+cd ../end-to-end-tests/.buildkite
 python3 drawing_wrapper.py ../../lmcache-tests/outputs/
