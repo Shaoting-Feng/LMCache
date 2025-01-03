afefe2189afdaae1fe567febca8d26e0ba77c99b
Shaoting <shaotingf@uchicago.edu>
2025-01-03 15:09:01 -0600

    add end to end tests on buildkite

---
+++ .buildkite/end-to-end-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../lmcache-tests
+python3 main.py tests/tests.py -f test_chunk_prefill -o outputs/
+python3 main.py tests/tests.py -f test_lmcache_local_gpu -o outputs/
+python3 main.py tests/tests.py -f test_lmcache_local_distributed -o outputs/
+python3 main.py tests/tests.py -f test_lmcache_remote_cachegen -o outputs/
+cd ../multi-round-qa/.buildkite
+python3 drawing_wrapper.py ../../lmcache-tests/outputs/
