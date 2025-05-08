faa5e4e54b703cd00b535df5b69235d546fee6d4
siddhantray <siddhant.r98@gmail.com>
2025-05-08 19:01:21 +0000

    Fig 3 updates samsum
    Signed-off-by: siddhantray <siddhant.r98@gmail.com>

--- serve/dataset/split.py
+++ serve/dataset/split.py
@@ -1,21 +1,38 @@
 import pandas as pd
+# import llama tokenizer
+from transformers import AutoTokenizer
 
-input_file = "samsum_processed.csv"
+input_file_1 = "samsum_processed.csv"
+input_file_2 = "qmsum_processed.csv"
 
-df = pd.read_csv(input_file)
+df = pd.read_csv(input_file_1)
 print(f"Original dataset size: {df.shape}")
+qdf = pd.read_csv(input_file_2)
+print(f"QMSUM dataset size: {qdf.shape}")
 
-## Split 1, df.length < 3000
-df1 = df[df['length'] < 3000]
-print(f"Dataset size with length < 3000: {df1.shape}")
-df1.to_csv("samsum_processed_lt_3000.csv", index=False)
+## Split 1, 5500 < df.length < 6500
+df1 = df[(df['length'] >= 5500) & (df['length'] < 6500)]
+print(f"Dataset size with 5500 < length < 6500: {df1.shape}")
+df1.to_csv("samsum_processed_bw_5500_6500.csv", index=False)
 
-## Split 2, 3000 < df.length < 10000    
-df2 = df[(df['length'] >= 3000) & (df['length'] < 10000)]
-print(f"Dataset size with 1000 < length < 10000: {df2.shape}")
-df2.to_csv("samsum_processed_bw_3000_10000.csv", index=False)
+## Split 2, 9000 < df.length < 10000
+df2 = df[(df['length'] >= 8500) & (df['length'] < 9500)]
+print(f"Dataset size with 8500 < length < 9500: {df2.shape}")
+df2.to_csv("samsum_processed_bw_8500_9500.csv", index=False)
 
-## Split 3, 10000 < df.length < 100000
-df3 = df[(df['length'] >= 10000) & (df['length'] < 100000)]
-print(f"Dataset size with 10000 < length < 100000: {df3.shape}")
-df3.to_csv("samsum_processed_bw_10000_100000.csv", index=False)
+## Split 3, 11500 < df.length < 12500
+df3 = df[(df['length'] >= 11500) & (df['length'] < 12500)]
+print(f"Dataset size with 11500 < length < 12500: {df3.shape}")
+df3.to_csv("samsum_processed_bw_11500_12500.csv", index=False)
+
+# for qdf, add the length column
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+qdf['length'] = qdf['context'].apply(lambda x: len(tokenizer(x)['input_ids']))
+# add column for dataset
+qdf['dataset'] = "qmsum"
+
+df2 = qdf[(qdf['length'] >= 9000) & (qdf['length'] < 10000)]
+print(f"QMSUM dataset size with 9000 < length < 10000: {df2.shape}")
+df2.to_csv("qmsum_processed_bw_9000_10000.csv", index=False)
+
+## Split 2, 
