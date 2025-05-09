e30f2488f634bfc1fc40f7d1c6af332a811fe577
Shaoting <shaotingf@uchicago.edu>
2025-05-09 18:20:33 +0000

    fig 3 part 2
    Signed-off-by: Shaoting <shaotingf@uchicago.edu>

--- serve/dataset/split.py
+++ serve/dataset/split.py
@@ -31,8 +31,17 @@ qdf['length'] = qdf['context'].apply(lambda x: len(tokenizer(x)['input_ids']))
 # add column for dataset
 qdf['dataset'] = "qmsum"
 
-df2 = qdf[(qdf['length'] >= 9000) & (qdf['length'] < 10000)]
-print(f"QMSUM dataset size with 9000 < length < 10000: {df2.shape}")
-df2.to_csv("qmsum_processed_bw_9000_10000.csv", index=False)
+## Split 1, 8500 < qdf.length < 9500
+df2 = qdf[(qdf['length'] >= 8500) & (qdf['length'] < 9500)]
+print(f"QMSUM dataset size with 8500 < length < 9500: {df2.shape}")
+df2.to_csv("qmsum_processed_bw_8500_9500.csv", index=False)
 
-## Split 2, 
+## Split 2, 13000 < qdf.length < 14000
+df3 = qdf[(qdf['length'] >= 13000) & (qdf['length'] < 14000)]
+print(f"QMSUM dataset size with 13000 < length < 14000: {df3.shape}")
+df3.to_csv("qmsum_processed_bw_13000_14000.csv", index=False)
+
+## Split 3, 17500 < qdf.length < 18500
+df4 = qdf[(qdf['length'] >= 17500) & (qdf['length'] < 18500)]
+print(f"QMSUM dataset size with 17500 < length < 18500: {df4.shape}")
+df4.to_csv("qmsum_processed_bw_17500_18500.csv", index=False)
